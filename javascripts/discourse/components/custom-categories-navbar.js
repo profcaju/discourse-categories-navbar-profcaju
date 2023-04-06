314ed5114dfe903a9b0afe8ed281522ccc7456e9
Prof. Caju <profcaju@tutorbrasil.com.br>
2023-04-06 00:06:33 -0300

    Update custom-categories-navbar.js

--- javascripts/discourse/components/custom-categories-navbar.js
+++ javascripts/discourse/components/custom-categories-navbar.js
@@ -16,10 +16,13 @@ export default class CustomCategoriesNavbar extends Component {
   setActiveSlug() {
     const currentRoute = this.router.currentRoute;
     Object.entries(currentRoute).forEach((keyValuePair) => {
-      console.log("  ", ...keyValuePair);
+      //   console.log("  ", ...keyValuePair);
     });
-    console.log("\n");
-    console.log("\n");
+    // console.log("\n");
+    // console.log("\n");
+    if (currentRoute && currentRoute.parent.name == "topic") {
+      console.log("entrei num TÓPICO\n");
+    }
     if (currentRoute && currentRoute.attributes?.category) {
       let activeCategory = currentRoute.attributes.category;
 
